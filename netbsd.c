91591ad51f973d3785122b9ea8f7b94010293184
Virtual- <luke@tonnochi.net>
2020-11-02 23:07:17 +0000

    NetBSD tests

--- netbsd.c
+++ netbsd.c
@@ -1,9 +1,17 @@
 #ifdef __NetBSD__
 #include <stdio.h>
+#include <sys/utsname.h>
+
+#include "heartbeat.h"
 
 void testNetBSD() 
 {
-    printf("This is NetBSD only!\n");
+    struct utsname uinfo;
+    uname(&uinfo);
+
+    printf("Version: %s %s\n", uinfo.sysname, uinfo.release);
+    printf("Packages:\n");
+    printf("Shell: %s\n", printEnv("SHELL"));
 }
 
 #endif
